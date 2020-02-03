e3df7aa1494263cce59ff2c97c7c1d714e80257f
mpacek <m.pacek@onet.eu>
2020-02-03 23:01:35 +0100

    26. Buttons in Action

--- rn-starter/rn-starter/src/screens/HomeScreen.js
+++ rn-starter/rn-starter/src/screens/HomeScreen.js
@@ -1,14 +1,19 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Yo!</Text>
+      <Button onPress={() => console.log('ok')} title="Go to Components Demo" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
+    fontSize: 30,
+  },
 });
 
 export default HomeScreen;
