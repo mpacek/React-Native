3af69efd61369ea89d515caf39cdeb87ae523a56
mpacek <m.pacek@onet.eu>
2020-02-04 16:17:21 +0100

    39 - Excercise

--- rn-starter/rn-starter/src/screens/HomeScreen.js
+++ rn-starter/rn-starter/src/screens/HomeScreen.js
@@ -15,6 +15,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate('List')}
         title="Go to List Demo"
       />
+      <Button
+        onPress={() => navigation.navigate('Image')}
+        title="Go to Image Demo"
+      />
     </View>
   );
 };
