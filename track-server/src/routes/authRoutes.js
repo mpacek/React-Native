f82fd048384021564f6d9982dd4a739398e8a1b0
mpacek <m.pacek@onet.eu>
2020-02-09 23:17:18 +0100

    176. Token

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const User = mongoose.model('User');
 
 const router = (require = express.Router());
 
-router.post('/signup', (req, res) => {
-  console.log(req.body);
-  res.send('Hi!');
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, 'MY_SECTET_KEY');
+    res.send({ token });
+    res.send('Hi!');
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
