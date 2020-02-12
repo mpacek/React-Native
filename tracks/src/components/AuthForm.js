451e5122daadb308e4a66d9a78c69ee226a8f4f6
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2020-02-12 14:34:28 +0100

    212. Wiring up signin

---
+++ tracks/src/components/AuthForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from './Spacer';
+
+const AuthForm = ({ headerText, errorMessage, onSubmit, submitButtonText }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <Spacer>
+        <Text h3>{headerText}</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      {errorMessage ? (
+        <Spacer>
+          <Text style={styles.errorMessage}>{errorMessage}</Text>
+        </Spacer>
+      ) : null}
+      <Spacer />
+      <Spacer>
+        <Button
+          title={submitButtonText}
+          onPress={() => onSubmit({ email, password })}
+        />
+      </Spacer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  errorMessage: {
+    fontSize: 16,
+    color: 'red',
+  },
+});
+
+export default AuthForm;
--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SigninScreen</Text>;
+  const { state, signin } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to Your Account"
+        errorMessage={state.errorMessage}
+        submitButtonText="Sign In"
+        onSubmit={signin}
+      />
+      <NavLink
+        routeName="Signup"
+        text="Dont have an account? Sign up instead"
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 150,
+  },
+});
 
 export default SigninScreen;
