a9b8dca39de31f96daf07a17bb062bf87b938290
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2020-02-13 14:59:53 +0100

    230. Location Context

--- tracks/src/components/Map.js
+++ tracks/src/components/Map.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React, { useContect } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { Polyline } from 'react-native-maps';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const Map = () => {
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-    points.push({
-      latitude: 37.33233 + i * 0.001,
-      longitude: -122.03121 + i * 0.001,
-    });
-  }
+  const { state } = useContext(LocationContext);
+
+  console.log(state);
 
   return (
     <MapView
@@ -20,9 +17,7 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapView>
+    ></MapView>
   );
 };
 
