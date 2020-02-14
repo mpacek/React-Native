da80c45501d911c9c74af18572f851e704076adb
PACKODELL\mpacek <m.pacek.1985@gmail.com>
2020-02-14 11:36:46 +0100

    255. Map Final

--- tracks/App.js
+++ tracks/App.js
@@ -11,7 +11,19 @@ import TrackListScreen from './src/screens/TrackListScreen';
 import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 import { Provider as AuthProvider } from './src/context/AuthContext';
 import { Provider as LocationProvider } from './src/context/LocationContext';
+import { Provider as TrackProvider } from './src/context/TrackContext';
 import { setNavigator } from './src/navigationRef';
+import { FontAwesome } from '@expo/vector-icons';
+
+const trackListFlow = createStackNavigator({
+  TrackList: TrackListScreen,
+  TrackDetail: TrackDetailScreen,
+});
+
+trackListFlow.navigationOptions = {
+  title: 'Tracks',
+  tabBarIcon: <FontAwesome name="th-list" size={20} />,
+};
 
 const switchNavigator = createSwitchNavigator({
   ResolveAuth: ResolveAuthScreen,
@@ -20,10 +32,7 @@ const switchNavigator = createSwitchNavigator({
     Signin: SigninScreen,
   }),
   mainFlow: createBottomTabNavigator({
-    trackListFlow: createStackNavigator({
-      TrackList: TrackListScreen,
-      TrackDetail: TrackDetailScreen,
-    }),
+    trackListFlow,
     TrackCreate: TrackCreateScreen,
     Account: AccountScreen,
   }),
@@ -33,14 +42,16 @@ const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
-    <LocationProvider>
-      <AuthProvider>
-        <App
-          ref={navigator => {
-            setNavigator(navigator);
-          }}
-        />
-      </AuthProvider>
-    </LocationProvider>
+    <TrackProvider>
+      <LocationProvider>
+        <AuthProvider>
+          <App
+            ref={navigator => {
+              setNavigator(navigator);
+            }}
+          />
+        </AuthProvider>
+      </LocationProvider>
+    </TrackProvider>
   );
 };
--- tracks/src/components/TrackForm.js
+++ tracks/src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
   const {
@@ -10,16 +11,12 @@ const TrackForm = () => {
     stopRecording,
     changeName,
   } = useContext(LocationContext);
+  const [saveTrack] = useSaveTrack();
 
   return (
     <>
-      <Spacer>
-        <Input
-          value={name}
-          onChangeText={changeName}
-          placeholder="Enter name"
-        />
-      </Spacer>
+      <Input value={name} onChangeText={changeName} placeholder="Enter name" />
+
       <Spacer>
         {recording ? (
           <Button title="Stop" onPress={stopRecording} />
@@ -27,6 +24,12 @@ const TrackForm = () => {
           <Button title="Start Recording" onPress={startRecording} />
         )}
       </Spacer>
+
+      {!recording && locations.length ? (
+        <Spacer>
+          <Button title="Save Recording" onPress={saveTrack} />
+        </Spacer>
+      ) : null}
     </>
   );
 };
